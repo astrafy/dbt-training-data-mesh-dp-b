d32cf0a92553bd9b70dcb19f735084cb556e23c6
Łukasz Ściga <lukasz@astrafy.io>
2023-05-31 17:02:18 +0200

    Add contract

--- dbt/models/hr/datamart/customers.sql
+++ dbt/models/hr/datamart/customers.sql
@@ -1,6 +1,11 @@
 with customers as (
 
-    select * from {{ ref('int_customers') }}
+    select
+      customer_id,
+      first_name,
+      last_name,
+      full_name
+    from {{ ref('int_customers') }}
 
 )
 
